323074bcad9718811b1ffe7a3e792d79fd0cec6f
RamS24 <AlSaadi.Rami24@gmail.com>
2022-11-29 16:23:42 +0100

    final sol - 4tasks

--- solution.js
+++ solution.js
@@ -1,8 +1,4 @@
 import stars from "./stars.js";
 
-// 3. Read the arguments from the terminal with `process.argv` and run the function you just imported, passing in those arguments
-// Path: solution.js
-// console.log(process.argv);
 stars(process.argv[2], process.argv[3]);
 
-// 4. Run your solution with `node solution.js 10 "hello world"`
